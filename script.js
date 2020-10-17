547292390745ae1122eaa44668efc4ddc3216f31
Papaya Delight <jasmineteters2020@gmail.com>
2020-10-17 15:15:03 -0600

    got TA help and Tutor help, added my for loop for getting the colors to change depending on time, and another for loop to save the text in the right textarea, fixed some ugly styling problems, added some comments

--- script.js
+++ script.js
@@ -1,53 +1,65 @@
 // Defining all planner time elements
-var am9 = $("#am9");
-var am10 = $("#am10");
-var am11 = $("#am11");
-var pm12 = $("#pm12");
-var pm1 = $("#pm1");
-var pm2 = $("#pm2");
-var pm3 = $("#pm3");
-var pm4 = $("#pm4");
-var pm5 = $("#pm5");
-saveBtn = $("#saveBtn")
-
-am9 = moment().hour('7').format("LT");
-
+var saveBtn = $("#saveBtn")
+var scheduleTest = $("#scheduledTest")
+var timeCheck = moment().format("H")
 var currentDay = $("#currentDay");
-
 var m = moment();
 currentDay.text(m.format("[Today is] dddd MMMM Do YYYY[.] [The time is] LT[.]"));
 
 
-var timeCheck = moment().format("LT")
-
-console.log(timeCheck)
-console.log(am9);
+var savedTimes = JSON.parse(localStorage.getItem("scheduleText")) || {}
 
 
 
 
 // Indicate if the time slot has passed IS SAME moment.js, color code and addClass past present or future
-// function hourSlot() {
+function hourSlot(element, time) {
+    if (time == timeCheck) {
+        element.addClass("present");
+    } else if (time < timeCheck) {
+        element.addClass("past");
+    } else(element.addClass("future"));
 
-// }
+}
 
+for (let index = 9; index < 18; index++) {
+    const time = index;
+    var element = $("#" + time)
+    hourSlot(element, time);
+
+}
 
 
 // onClick of time block, add text input
-// function addToScheduleText() {
+function addToScheduleText() {
 
-// }
+}
 
+$(".saveBtn").on('click', saveScheduledEvent)
 
-
+var savedSchedule = $(this).parent().parent().find("textarea").val();
 
 // save button saves to localStorage
 function saveScheduledEvent(event) {
     event.preventDefault();
-    if (saveBtn.on('click'))
-        localStorage.setItem("scheduledTask", JSON.stringify(values));
+    var savedScheduleText = $(this).parent().parent().find("textarea").val();
+    var savedTimeBlock = $(this).parent().parent().find("textarea").attr("id");
+
+    savedTimes[savedTimeBlock] = savedScheduleText;
+    localStorage.setItem("scheduleText", JSON.stringify(savedTimes))
+    console.log("it works")
+    console.log($(this).parent().parent().find("textarea").attr("id"))
 }
 
 
 
 // saved item persists if page is refreshed
+
+function getEvents() {
+    for (const key in savedTimes) {
+        $("#" + key).val(savedTimes[key])
+        console.log(`${key}: ${savedTimes[key]}`);
+    }
+}
+
+getEvents();
